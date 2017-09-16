e5ea85b02a9147489313880e161172f38e568667
Omar Chehab <omarchehab98@gmail.com>
2017-09-16 12:45:33 -0400

    Update ActivitySinglePage test

--- app/containers/ActivitySinglePage/tests/index.test.js
+++ app/containers/ActivitySinglePage/tests/index.test.js
@@ -4,9 +4,15 @@ import { shallow } from 'enzyme';
 import ActivitySinglePage from '../index';
 
 describe('<ActivitySinglePage />', () => {
-  it('should render the page message', () => {
+  it('should render an activity page', () => {
     shallow(
-      <ActivitySinglePage />
+      <ActivitySinglePage params={{ id: 1 }} />
+    );
+  });
+
+  it('should render should fail to render activity page', () => {
+    shallow(
+      <ActivitySinglePage params={{ id: 'abc' }} />
     );
   });
 });

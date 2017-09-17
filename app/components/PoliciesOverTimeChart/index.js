aa554dbff785330032e73d8762bdc21ff22d6226
Omar Chehab <omarchehab98@gmail.com>
2017-09-17 08:09:58 -0400

    Add ZoomIn and ZoomOut functionality for Line Chart

--- app/components/PoliciesOverTimeChart/index.js
+++ app/components/PoliciesOverTimeChart/index.js
@@ -3,24 +3,24 @@ import PropTypes from 'prop-types';
 import { chain, min, max } from 'lodash';
 import { Line as LineChart } from 'react-chartjs-2';
 import 'chartjs-plugin-annotation';
+import ZoomIn from 'material-ui-icons/ZoomIn';
+import ZoomOut from 'material-ui-icons/ZoomOut';
 
 const DAYS = 24 * 60 * 60 * 1000;
-const ACTIVITY_PRE_DURATION = 5;
-const ACTIVITY_DURATION = 20;
 
-function mapPolicyDistribution(activityDate, distribution) {
+function mapPolicyDistribution(activityDate, distribution, activityPreDuration, activityDuration) {
   const firstDate = new Date(
     min(
       Object.keys(distribution)
         .map((x) => new Date(x).getTime())
-        .concat([activityDate.getTime() - (ACTIVITY_PRE_DURATION * DAYS)])
+        .concat([activityDate.getTime() - (activityPreDuration * DAYS)])
     )
   );
   const lastDate = new Date(
     max(
       Object.keys(distribution)
         .map((x) => new Date(x).getTime())
-        .concat([activityDate.getTime() + (ACTIVITY_DURATION * DAYS)])
+        .concat([activityDate.getTime() + (activityDuration * DAYS)])
     )
   );
 
@@ -41,9 +41,9 @@ function mapPolicyDistribution(activityDate, distribution) {
       .indexOf(activityDate.toISOString().split('T')[0]);
 
   const labels = Object.keys(distributionSorted)
-    .slice(activityDateIndex - ACTIVITY_PRE_DURATION, activityDateIndex + ACTIVITY_DURATION);
+    .slice(activityDateIndex - activityPreDuration, activityDateIndex + activityDuration);
   const values = Object.values(distributionSorted)
-    .slice(activityDateIndex - ACTIVITY_PRE_DURATION, activityDateIndex + ACTIVITY_DURATION);
+    .slice(activityDateIndex - activityPreDuration, activityDateIndex + activityDuration);
 
   return {
     labels,
@@ -51,90 +51,187 @@ function mapPolicyDistribution(activityDate, distribution) {
   };
 }
 
-function PoliciesOverTimeChart(props) {
-  const {
-    activity,
-    policyDistribution,
-    policyWithPromoDistribution,
-  } = props;
+const MIN_ACTIVITY_PRE = 5;
+const MAX_ACTIVITY_PRE = 30;
+const ADD_ACTIVITY_PRE = 5;
 
-  const activityDate = new Date(activity.activity_date);
+const MIN_ACTIVITY_DURATION = 20;
+const MAX_ACTIVITY_DURATION = 60;
+const ADD_ACTIVITY_DURATION = 10;
 
-  const mappedPolicyDistribution = mapPolicyDistribution(activityDate, policyDistribution);
-  const mappedPolicyWithPromoDistribution = mapPolicyDistribution(activityDate, policyWithPromoDistribution);
+class PoliciesOverTimeChart extends React.Component {
+  state = {
+    activityPreDuration: 5,
+    activityDuration: 20,
+  }
 
-  const maxValue = max(mappedPolicyDistribution.values) || 2;
-  const upperBound = Math.ceil((maxValue + (maxValue * 0.15)) / 100) * 100;
+  handleZoomOut = () => {
+    this.setState((prev) => ({
+      activityPreDuration: Math.max(
+        MIN_ACTIVITY_PRE,
+        Math.min(
+          MAX_ACTIVITY_PRE,
+          prev.activityPreDuration + ADD_ACTIVITY_PRE
+        )
+      ),
+      activityDuration: Math.max(
+        MIN_ACTIVITY_DURATION,
+        Math.min(
+          MAX_ACTIVITY_DURATION,
+          prev.activityDuration + ADD_ACTIVITY_DURATION
+        )
+      ),
+    }));
+  }
 
-  return (
-    <LineChart
-      data={{
-        labels: mappedPolicyDistribution.labels,
-        datasets: [{
-          data: mappedPolicyWithPromoDistribution.values,
-          backgroundColor: '#F48FB1',
-          borderColor: '#C2185B',
-          label: 'Conversions using Promotion',
-        }, {
-          data: mappedPolicyDistribution.values,
-          backgroundColor: '#81D4FA',
-          borderColor: '#0288D1',
-          label: 'Total Conversions',
-        }],
-      }}
-      options={{
-        annotation: {
-          annotations: [{
-            type: 'line',
-            mode: 'vertical',
-            scaleID: 'x-axis-0',
-            value: activityDate.toISOString().split('T')[0],
-            borderColor: 'red',
-            label: {
-              content: 'Activity Start',
-              enabled: true,
-              position: 'top',
-            },
-          },
-          ] },
-        legend: {
-          display: true,
-          position: 'right',
-        },
-        scales: {
-          xAxes: [{
-            scaleLabel: {
+  handleZoomIn = () => {
+    this.setState((prev) => ({
+      activityPreDuration: Math.max(
+        MIN_ACTIVITY_PRE,
+        Math.min(
+          MAX_ACTIVITY_PRE,
+          prev.activityPreDuration - ADD_ACTIVITY_PRE
+        )
+      ),
+      activityDuration: Math.max(
+        MIN_ACTIVITY_DURATION,
+        Math.min(
+          MAX_ACTIVITY_DURATION,
+          prev.activityDuration - ADD_ACTIVITY_DURATION
+        )
+      ),
+    }));
+  }
+
+  render() {
+    const {
+      activity,
+      policyDistribution,
+      policyWithPromoDistribution,
+    } = this.props;
+    const {
+      activityPreDuration,
+      activityDuration,
+    } = this.state;
+
+    const activityDate = new Date(activity.activity_date);
+
+    const mappedPolicyDistribution = mapPolicyDistribution(activityDate, policyDistribution, activityPreDuration, activityDuration);
+    const mappedPolicyWithPromoDistribution = mapPolicyDistribution(activityDate, policyWithPromoDistribution, activityPreDuration, activityDuration);
+
+    const maxValue = max(mappedPolicyDistribution.values) || 2;
+    const upperBound = Math.ceil((maxValue + (maxValue * 0.15)) / 100) * 100;
+
+    return (
+      <div style={{ position: 'relative' }}>
+        <LineChart
+          data={{
+            labels: mappedPolicyDistribution.labels,
+            datasets: [{
+              data: mappedPolicyWithPromoDistribution.values,
+              backgroundColor: '#F48FB1',
+              borderColor: '#C2185B',
+              label: 'Conversions using Promotion',
+            }, {
+              data: mappedPolicyDistribution.values,
+              backgroundColor: '#81D4FA',
+              borderColor: '#0288D1',
+              label: 'Total Conversions',
+            }],
+          }}
+          options={{
+            annotation: {
+              annotations: [{
+                type: 'line',
+                mode: 'vertical',
+                scaleID: 'x-axis-0',
+                value: activityDate.toISOString().split('T')[0],
+                borderColor: 'red',
+                label: {
+                  content: 'Activity Start',
+                  enabled: true,
+                  position: 'top',
+                },
+              },
+              ] },
+            legend: {
               display: true,
-              labelString: 'Date',
-              fontSize: 20,
-              fontStyle: 'bold',
+              position: 'right',
             },
-            ticks: {
-              maxRotation: 45,
-              minRotation: 45,
+            scales: {
+              xAxes: [{
+                scaleLabel: {
+                  display: true,
+                  labelString: 'Date',
+                  fontSize: 20,
+                  fontStyle: 'bold',
+                },
+                ticks: {
+                  maxRotation: 45,
+                  minRotation: 45,
+                },
+              }],
+              yAxes: [{
+                scaleLabel: {
+                  display: true,
+                  labelString: 'Conversions',
+                  fontSize: 20,
+                  fontStyle: 'bold',
+                },
+                ticks: {
+                  stepSize: Math.round(maxValue / 10 / 100) * 100,
+                  max: upperBound,
+                  min: 0,
+                } }],
             },
-          }],
-          yAxes: [{
-            scaleLabel: {
+            title: {
               display: true,
-              labelString: 'Conversions',
-              fontSize: 20,
-              fontStyle: 'bold',
+              fontSize: 30,
+              text: 'Conversions per day',
             },
-            ticks: {
-              stepSize: Math.round(maxValue / 10 / 100) * 100,
-              max: upperBound,
-              min: 0,
-            } }],
-        },
-        title: {
-          display: true,
-          fontSize: 30,
-          text: 'Conversions per day',
-        },
-      }}
-    />
-  );
+          }}
+        />
+        <button
+          style={{
+            position: 'absolute',
+            right: '30px',
+            bottom: '30px',
+            background: 'none',
+            border: 'none',
+            width: '40px',
+            height: '40px',
+          }}
+          onClick={this.handleZoomIn}
+        >
+          <ZoomIn
+            style={{
+              width: '40px',
+              height: '40px',
+            }}
+          />
+        </button>
+        <button
+          style={{
+            position: 'absolute',
+            right: '90px',
+            bottom: '30px',
+            background: 'none',
+            border: 'none',
+            width: '40px',
+            height: '40px',
+          }}
+          onClick={this.handleZoomOut}
+        >
+          <ZoomOut
+            style={{
+              width: '40px',
+              height: '40px',
+            }}
+          />
+        </button>
+      </div>
+    );
+  }
 }
 
 
